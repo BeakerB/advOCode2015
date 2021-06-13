d0c0aedefaa07fc4620e0cac2685072eefe2e071
BeakerB <Beaker.Bunsen@outlook.de>
2021-06-13 16:47:37 +0200

    Teil 2 der Aufgabe

--- paperOrder.py
+++ paperOrder.py
@@ -1,7 +1,12 @@
 def kleinsteFläche(liste):
     liste = sorted(liste)
     return liste[0] * liste[1]
+
+def kleinsterUmfang(liste):
+    liste = sorted(liste)
+    return 2 * (liste[0] + liste[1])
     
+bandLänge = 0
 gesamtFläche = 0
 daten = open('wrappingPaper.txt','r')
 for zeile in daten:
@@ -13,5 +18,7 @@ for zeile in daten:
     oberfläche = 2 * (breite * länge + breite * höhe + länge * höhe)
     bestellFläche = oberfläche + kleinsteFläche([länge, breite, höhe])
     gesamtFläche += bestellFläche
+    bandLänge += länge * höhe * breite + kleinsterUmfang([länge, breite, höhe])
 daten.close()
 print(gesamtFläche)
+print(bandLänge)
